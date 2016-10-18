cb2aa30c69aa04442e8f76964f6066989162957a
vimalaad <vimalaad@gmail.com>
2016-10-18 18:42:40 +0330

    do not build .+

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,2 @@
 Pkg.clone("https://github.com/DANA-Laboratory/DanaTypes.jl.git")
 Pkg.build("DanaTypes")
-Pkg.clone("https://github.com/DANA-Laboratory/DotPlusInheritance.jl.git")
-Pkg.build("DotPlusInheritance")
